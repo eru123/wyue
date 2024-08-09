924cfecd7c3c9389b462717a151b198ac8b5a970
eru123 <yeoligoakino@gmail.com>
2024-08-09 12:42:39 +0800

    added Helper class

---
+++ src/Helper.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Wyue;
+
+use Exception;
+
+/**
+ * Wyue Helper class
+ */
+class Helper
+{
+    /**
+     * Converts argument to a callable
+     * @param mixed $cb Callback
+     * @return callable
+     */
+    final public static function MakeCallable($cb): callable
+    {
+        if (is_callable($cb)) {
+            return $cb;
+        }
+
+        if (is_string($cb)) {
+            $rgx = '/^([a-zA-Z0-9_\\\\]+)(::|@)([a-zA-Z0-9_]+)$/';
+            if (preg_match($rgx, $cb, $matches)) {
+                $classname = $matches[1];
+                $method = $matches[3];
+                if (class_exists($classname)) {
+                    $obj = new $classname();
+                    if (method_exists($obj, $method)) {
+                        return [$obj, $method];
+                    }
+                }
+            }
+        }
+
+        if (is_array($cb) && count($cb) == 2) {
+            if (is_object($cb[0]) && is_string($cb[1])) {
+                return $cb;
+            } else if (is_string($cb[0]) && is_string($cb[1])) {
+                $classname = $cb[0];
+                $method = $cb[1];
+                if (class_exists($classname)) {
+                    $obj = new $classname();
+                    if (method_exists($obj, $method)) {
+                        return [$obj, $method];
+                    } else if (method_exists($classname, $method)) {
+                        return $cb;
+                    }
+                }
+            }
+        }
+
+        throw new Exception('Invalid callback');
+    }
+
+    /**
+     * Calls a callable with arguments
+     * @param mixed $cb Callback
+     * @param array $args
+     * @return mixed
+     */
+    final public static function Callback($cb, array $args = [])
+    {
+        return call_user_func_array(static::MakeCallable($cb), $args);
+    }
+
+    /**
+     * Combines one or more URL paths
+     * @param string ...$paths
+     * @return string
+     */
+    final public static function CombineUrlPaths(string ...$paths): string
+    {
+        $paths = array_filter($paths, function ($path) {
+            return !empty(trim($path, " \n\r\t\v\0\/")) && !!preg_match('//u', $path);
+        });
+
+        $paths = array_map(function ($path) {
+            return trim($path, " \n\r\t\v\0\/");
+        }, $paths);
+
+        $path = implode('/', $paths);
+
+        if (substr($path, -1) == '/') {
+            $path = substr($path, 0, -1);
+        }
+
+        return "/" . $path;
+    }
+}
