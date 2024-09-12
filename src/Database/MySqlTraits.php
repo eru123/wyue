915d27abaa7bf126996645f4c4be01263f663acb
eru123 <yeoligoakino@gmail.com>
2024-09-12 19:55:22 +0800

    implement mysql traits

---
+++ src/Database/MySqlTraits.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wyue\Database;
+
+use PDOStatement;
+use Wyue\MySql;
+
+trait MySqlTraits
+{
+
+    /**
+     * Executes the SQL and return the PDOStatement
+     * @param string $sql The SQL to execute
+     * @param array $params The parameters to bind
+     * @param bool $dryrun Dryrun mode, returns MySql Object instead of PDOStatement
+     * @return PDOStatement|MySql Returns MySql Object if dryrun is true, else returns PDOStatement
+     */
+    public function query(string $sql, array $params = [], bool $dryrun = false): MySql|PDOStatement
+    {
+        if ($dryrun) {
+            return new MySql($sql, $params);
+        }
+
+        return (new MySql($sql, $params))->exec();
+    }
+
+    /**
+     * Executes the SQL and return the number of rows affected
+     * @param string $sql The SQL to execute
+     * @param array $params The parameters to bind
+     * @param bool $dryrun Dryrun mode, returns MySql Object instead of PDOStatement
+     * @return int|MySql Returns MySql Object if dryrun is true, else returns number of rows affected
+     */
+    public function execute(string $sql, array $params = [], bool $dryrun = false): MySql|int
+    {
+        $result = $this->query($sql, $params, $dryrun);
+        return $result instanceof PDOStatement ? $result->rowCount() : $result;
+    }
+}
