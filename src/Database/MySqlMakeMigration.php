8039af8ce10f51e517da320ecb7afee6dc789ed3
eru123 <yeoligoakino@gmail.com>
2024-10-06 15:20:13 +0800

    added models to make migration

--- src/Database/MySqlMakeMigration.php
+++ src/Database/MySqlMakeMigration.php
@@ -9,6 +9,7 @@ use Wyue\Commands\CLI;
 use Wyue\Commands\AbstractCommand;
 use Wyue\Database\AbstractMigration;
 use Wyue\Format;
+use Wyue\MySql;
 
 class MySqlMakeMigration extends AbstractCommand
 {
@@ -21,16 +22,15 @@ class MySqlMakeMigration extends AbstractCommand
     ];
 
     protected array $options = [
-        'm|model' => 'Create migration with model.',
         'd|dir' => 'The directory where the migration file will be created.',
         't|table' => 'The table name for the migration.',
     ];
 
     protected array $flags = [
+        'm|model' => 'Create migration with model with same class name.',
         'f|force' => 'Force override existing migration file if happens to have a same class name.',
         'V|verbose' => 'Verbose output.',
     ];
-
 
     public function handle()
     {
@@ -114,9 +114,70 @@ class MySqlMakeMigration extends AbstractCommand
             CLI::success("SUCCESS: Migration file created");
             CLI::success("Class: " . $fclassnme);
             CLI::success("File: " . $file);
+
+            if ($this->flag('m|model')) {
+                $this->createModel($classname, $this->opt('t|table', $filemname));
+            }
             exit(0);
         } else {
             throw new Exception('Make Migration Error: Failed to create migration file');
+        }
+    }
+
+    public function createModel(string $classname, string $table)
+    {
+        $namespace = MySql::getModelsNamespace();
+        $dir = MySql::getModelsPath();
+        if (!is_dir($dir)) {
+            if (!mkdir($dir, 0777, true)) {
+                throw new Exception('Make Model Error: Can\'t create model directory');
+            }
+        }
+
+        if (!is_writable($dir)) {
+            throw new Exception('Make Model Error: Can\'t write to model directory');
+        }
+
+        $dir = realpath($dir);
+
+        if (!$dir || !is_dir($dir)) {
+            throw new Exception('Make Model Error: Failed to get absolute path of models directory or \'' . MySql::getModelsPath() . '\' is not a directory');
+        }
+
+        $file = $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+
+        $tpl = <<<PHP
+        <?php
+
+        namespace {ModelNamespace};
+
+        use Wyue\Database\AbstractModel;
+
+        class {ModelName} extends AbstractModel
+        {
+            protected null|string \$table = '{ModelTable}';
+            protected null|array \$fillable = null;
+            protected null|array \$hidden = null;
+            protected null|string|int \$primaryKey = null;
+        }
+         
+        PHP;
+
+        $content = Format::template($tpl, [
+            'ModelNamespace' => $namespace,
+            'ModelName' => $classname,
+            'ModelTable' => $table
+        ], FORMAT_TEMPLATE_CURLY);
+
+        $success = file_put_contents($file, $content);
+
+        if ($success) {
+            CLI::success("SUCCESS: Model file created");
+            CLI::success("Class: $namespace\\$classname");
+            CLI::success("File: " . $file);
+            exit(0);
+        } else {
+            throw new Exception('Make Model Error: Failed to create model file');
         }
     }
 }
